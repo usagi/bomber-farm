342a34e425d65aaf8de6a84cae26261cf980a581
Usagi Ito <usagi@WonderRabbitProject.net>
2014-06-14 23:55:01 +0900

    refactoring

---
+++ source/server/initialize.php
@@ -0,0 +1,15 @@
+<?php namespace wonder_rabbit_project\bomber_farm\main;
+
+date_default_timezone_set( 'Asia/Tokyo' );
+
+require_once( 'logger.php' );
+use \wonder_rabbit_project\logger\logger;
+logger::file( 'bomber-farm' );
+logger::level( 'DEBUG' );
+
+require_once( 'exception.php' );
+use \wonder_rabbit_project\exception\exception;
+exception::error_to_exception( true );
+
+require_once( 'vec.php' );
+use \wonder_rabbit_project\math\vec;
--- source/server/main.php
+++ source/server/main.php
@@ -2,18 +2,8 @@
 
 const module_name = 'wonder_rabbit_project::bomber_farm::main';
 
-date_default_timezone_set( 'Asia/Tokyo' );
-
-require_once( 'logger.php' );
+require_once( 'initialize.php' );
 use \wonder_rabbit_project\logger\logger;
-logger::file( 'bomber-farm' );
-logger::level( 'DEBUG' );
-
-require_once( 'exception.php' );
-use \wonder_rabbit_project\exception\exception;
-exception::error_to_exception( true );
-
-require_once( 'vec.php' );
 use \wonder_rabbit_project\math\vec;
 
 $a = new vec( 3, 4 );
